ba52a34f0dd43e7be3df13a79cf13c3b29699c98
Leonardo <leonardo.diniz95@yahoo.com.br>
2018-11-13 18:51:31 -0200

    Trocar value do input

--- javascript/js/arquivo.js
+++ javascript/js/arquivo.js
@@ -1,6 +1,7 @@
 var nome;
 nome= "Jão do Pão";
 var idade = 2018 - 1980;
+var verificador = 0;
 
 function mensagem(){
     alert("Olha a mensagem");
@@ -27,6 +28,19 @@ function seuNome(){
 
 function clicaBotao(){
     alert("Clicou no botão.");
+}
+
+function alterarVisibilidade(){
+
+    if(verificador == 0){
+        document.getElementById("botao").style.display="none";
+        document.getElementById("visivel").value ="Apareça";
+        verificador = 1;
+    }else{
+        document.getElementById("botao").style.display="block";
+        document.getElementById("visivel").value ="Sumiu";
+        verificador = 0;
+    }
 }
 
 mensagem();
