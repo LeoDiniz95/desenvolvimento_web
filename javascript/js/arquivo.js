53d8acac347d2f968c224281fd078a5727246e70
Leonardo <leonardo.diniz95@yahoo.com.br>
2018-11-16 02:43:07 -0200

    apply e call

--- javascript/js/arquivo.js
+++ javascript/js/arquivo.js
@@ -37,13 +37,41 @@ function alterarVisibilidade(){
         document.getElementById("visivel").value ="Apareça";
         verificador = 1;
     }else{
-        document.getElementById("botao").style.display="block";
+        document.getElementById("botao").style.display="flex";
         document.getElementById("visivel").value ="Sumiu";
         verificador = 0;
     }
 }
 
+
+function algumaCoisa(){
+    
+    nome1 ={
+
+         idade: 52,
+         jao: function(nome){
+             return( "Nome: "+nome+". Idade: "+this.idade+".");
+         }
+
+        }
+
+    outro = {
+        idade: 43
+    }
+    
+    
+    
+
+    document.getElementById("label").innerHTML=nome1.jao('sss');;
+    document.getElementById("label").style.display="flex";
+    
+    alert(nome1.jao.call(outro,'sss'));
+
+    alert(nome1.jao.apply(outro,['buia']));
+
+}
+/*
 mensagem();
 maioridade();
 seuNome();
-maioridade();
+maioridade();*/
