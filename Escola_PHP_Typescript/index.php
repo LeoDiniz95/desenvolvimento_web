bba4769a9737a825c6660e8a8e2768e881d30dca
Leonardo <leonardo.diniz95@yahoo.com.br>
2018-12-22 23:31:21 -0200

    Experimentando OOP com TS(inacabado)

---
+++ Escola_PHP_Typescript/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+
+        <script type="text/javascript" src="js/classes.js">
+        </script>
+
+        <?php include('php/classes.php');?>
+
+    </head>
+
+</html>
